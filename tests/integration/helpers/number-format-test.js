e85f6f7a1d0bf62afd9dc966b088e88a730d3205
Layne Tcheng <laynegt@gmail.com>
2022-06-07 21:37:11 -0400

    ember 4

--- tests/integration/helpers/number-format-test.js
+++ tests/integration/helpers/number-format-test.js
@@ -3,15 +3,15 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
-module('Integration | Helper | number-format', function(hooks) {
+module('Integration | Helper | number-format', function (hooks) {
   setupRenderingTest(hooks);
 
   // TODO: Replace this with your real tests.
-  test('it renders', async function(assert) {
-    this.set('inputValue', '1234');
+  test('it renders', async function (assert) {
+    this.set('inputValue', 1234);
 
-    await render(hbs`{{number-format inputValue}}`);
+    await render(hbs`{{number-format this.inputValue}}`);
 
-    assert.equal(this.element.textContent.trim(), '1,234');
+    assert.dom(this.element).hasText('1,234');
   });
 });
