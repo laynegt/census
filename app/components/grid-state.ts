634a0700c89d584d23748062c63e7a69e6f49e32
Layne Tcheng <laynegt@gmail.com>
2022-06-17 14:36:49 -0400

    some ts conversion

---
+++ app/components/grid-state.ts
@@ -0,0 +1,28 @@
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { htmlSafe } from '@ember/template';
+import { interpolateRdYlBu } from 'd3-scale-chromatic';
+
+// import { SafeString } from '@ember/template';
+// this is a HACK? https://github.com/typed-ember/ember-cli-typescript/issues/1155
+import { SafeString } from 'handlebars';
+
+interface GridStateSignature {
+  Args: {
+    maxRank: number;
+  };
+}
+
+export default class GridStateComponent extends Component<GridStateSignature> {
+  @action
+  filterRanks(maxApportioned: number, rank: number): boolean {
+    return rank < maxApportioned;
+  }
+
+  @action
+  colorScale(rank: number): SafeString {
+    return htmlSafe(
+      'background-color: ' + interpolateRdYlBu(rank / (this.args.maxRank - 1))
+    );
+  }
+}
